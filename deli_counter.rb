cc3f6bc257751385e33e56d30fca290bcdc32f63
Eric Clark <eclark0013@gmail.com>
2019-06-10 19:27:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli=[]
+
+def line(array)
+if array.count==0
+  "The line is currently empty."
+  numbered_customer_list=""
+else
+  counter=1
+  array.each do |customer|
+    numbered_customer_list="numbered_customer_list"+"#{counter}. #{customer}"
+  end
+  "The line is currently: #{numbered_customer_list}"
+  end
+end
